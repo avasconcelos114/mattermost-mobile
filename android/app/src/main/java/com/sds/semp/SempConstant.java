ac0f5094fc7d09237ef8a7294cf100b2c0ad07fe
dylan.kwon <dylan.kwon98@gmail.com>
2018-11-20 13:22:44 +0900

    DS Block 작업 완료

---
+++ android/app/src/main/java/com/sds/semp/SempConstant.java
@@ -0,0 +1,42 @@
+package com.sds.semp;
+
+public class SempConstant {
+
+    /**
+     * DS CHECK
+     */
+    public static final String SCHEME = "https";
+
+    public static final String HOST = "www.samsungsmartoffice.net";
+
+    public static final String PATH = "kms/jsp/wagle/mobile/common/semp/MobileMosaicSempDsCheck.jsp";
+
+    public static final String QUERY_KNOX_ID = "knoxId";
+
+
+
+
+    /**
+     * SEMP Check
+     */
+    public static final String CONNECTION_TYPE = "https";
+
+    public static final String PORT_NUMBER = "443";
+
+    public static final String S_TYPE = "db";
+
+    public static final String DATA_TYPE = "json";
+
+    public static final String CONTEXT_URL = "semp4";
+
+    public static final String PARAM_ENCRYPTED = "true";
+
+    public static final String PARAM_COMPRESSED = "true";
+
+    public static final String USER_ADVANCE_KEY = "true";
+
+    public static final String S_CODE = "select_EMM_isActivated";
+
+    public static final String E_KEY_TYPE = "1";
+
+}
